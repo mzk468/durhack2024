2d3c28fea22eea0f4b0e895148b700dfb284dbf7
Zakariya <mzkhaled468@gmail.com>
2024-11-02 18:25:19 +0000

    Completed simple desktop view

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { Cross, Microphone } from "akar-icons"
+import { useState } from "react"
+import css from "./styles/App.module.css"
+
+export default function App() {
+    const [whoIsSpeaking, setWhoIsSpeaking] = useState<string | null>("donald")
+    const [isRecording, setIsRecording] = useState<boolean>(false)
+
+    const handleMicButtonPress = () => {
+        if (whoIsSpeaking) setIsRecording(false);
+        else setIsRecording(!isRecording);
+        if (!isRecording) setWhoIsSpeaking(null);
+    }
+
+    return <div id={css.root}>
+        {whoIsSpeaking ?
+            <img src={`/${whoIsSpeaking}.jpg`} />
+            :
+            <div id={css.presidentsCont}>
+                <img src="/joe.jpg" />
+                <img src="/donald.jpg" />
+                <img src="/barack.jpg" />
+            </div>
+        }
+        <button id={css.recordButton} onClick={handleMicButtonPress}>
+            {isRecording || whoIsSpeaking ? <Cross strokeWidth={3} size={48} color="white" /> : <Microphone strokeWidth={3} size={48} color="white" />}
+        </button>
+    </div>
+}
